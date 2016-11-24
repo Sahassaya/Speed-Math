acd7eb4a9654245abac8add4b41a2981faffe78b
baifern <fernzaa102@gmail.com>
2016-11-24 13:50:34 +0700

    menu

--- app/src/main/java/rtc/jeeranun/sahassaya/speedmath/MainActivity.java
+++ app/src/main/java/rtc/jeeranun/sahassaya/speedmath/MainActivity.java
@@ -1,13 +1,33 @@
 package rtc.jeeranun.sahassaya.speedmath;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicitการประกาศตัวแปร
+    private ImageView imageView;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        //image controller
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, PlayActivity.class));
+            }
+        });
+
+    } // Main Method
+} // Main Class
