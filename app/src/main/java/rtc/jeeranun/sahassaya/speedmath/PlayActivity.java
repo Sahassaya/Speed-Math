a461adb3191707896a15307c695a9c4166bba10c
baifern <fernzaa102@gmail.com>
2016-11-25 16:26:29 +0700

    Finish25/11/59

--- app/src/main/java/rtc/jeeranun/sahassaya/speedmath/PlayActivity.java
+++ app/src/main/java/rtc/jeeranun/sahassaya/speedmath/PlayActivity.java
@@ -2,12 +2,25 @@ package rtc.jeeranun.sahassaya.speedmath;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class PlayActivity extends AppCompatActivity {
+
+    //Explicit
+    private TextView questionTextView, ch1TextView,ch2TextView,ch3TextView, ch4TextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+
+
+
+        // Bind Widger
+        questionTextView = (TextView) findViewById(R.id.textView7);
+        ch1TextView = (TextView) findViewById(R.id.textView3);
+        ch2TextView = (TextView) findViewById(R.id.textView4);
+        ch3TextView = (TextView) findViewById(R.id.textView5);
+        ch4TextView = (TextView) findViewById(R.id.textView6);
     }
 }
