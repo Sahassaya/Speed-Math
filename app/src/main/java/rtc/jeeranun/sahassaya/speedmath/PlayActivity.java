6a087ffda61e3289227d790b3f98b8e44d3c3007
masterUNG <phrombutr@gmail.com>
2016-12-03 00:29:37 +0700

    Choose Active OK

--- app/src/main/java/rtc/jeeranun/sahassaya/speedmath/PlayActivity.java
+++ app/src/main/java/rtc/jeeranun/sahassaya/speedmath/PlayActivity.java
@@ -1,20 +1,22 @@
 package rtc.jeeranun.sahassaya.speedmath;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
-public class PlayActivity extends AppCompatActivity {
+public class PlayActivity extends AppCompatActivity implements View.OnClickListener {
 
     //Explicit
-    private TextView questionTextView, ch1TextView,ch2TextView,ch3TextView, ch4TextView;
+    private TextView questionTextView, ch1TextView, ch2TextView,
+            ch3TextView, ch4TextView;
+    private int chooseAnswerAnInt = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
-
-
 
         // Bind Widger
         questionTextView = (TextView) findViewById(R.id.textView7);
@@ -22,5 +24,38 @@ public class PlayActivity extends AppCompatActivity {
         ch2TextView = (TextView) findViewById(R.id.textView4);
         ch3TextView = (TextView) findViewById(R.id.textView5);
         ch4TextView = (TextView) findViewById(R.id.textView6);
+
+        //Click Controller
+        ch1TextView.setOnClickListener(this);
+        ch2TextView.setOnClickListener(this);
+        ch3TextView.setOnClickListener(this);
+        ch4TextView.setOnClickListener(this);
+
+    }   // Main Method
+
+    @Override
+    public void onClick(View view) {
+
+
+        switch (view.getId()) {
+            case R.id.textView3:
+                checkAnswer(Integer.parseInt(ch1TextView.getText().toString()));
+                break;
+            case R.id.textView4:
+                checkAnswer(Integer.parseInt(ch2TextView.getText().toString()));
+                break;
+            case R.id.textView5:
+                checkAnswer(Integer.parseInt(ch3TextView.getText().toString()));
+                break;
+            case R.id.textView6:
+                checkAnswer(Integer.parseInt(ch4TextView.getText().toString()));
+                break;
+        }   // switch
+
+    }   // onClick
+
+    private void checkAnswer(int intChoice) {
+        Log.d("3decV1", "You Choose Answer ==> " + intChoice);
     }
-}
+
+}   // Main Class
