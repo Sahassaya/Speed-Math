fa304a1702c6bf7a42810e04101cca235b1b2d1d
masterUNG <phrombutr@gmail.com>
2016-12-03 08:49:38 +0700

    new test

--- app/src/main/java/rtc/jeeranun/sahassaya/speedmath/PlayActivity.java
+++ app/src/main/java/rtc/jeeranun/sahassaya/speedmath/PlayActivity.java
@@ -22,6 +22,7 @@ public class PlayActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
+
         //Setup
         random = new Random();
 
